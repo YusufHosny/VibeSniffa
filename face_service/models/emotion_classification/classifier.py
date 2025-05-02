b741f4920c9efaee857dd3d3dd8bce1ea1f3e283
Yusuf Hosny <yusufyy2004@hotmail.com>
2025-05-02 13:21:29 +0200

    removed unnecessary processor

--- face_service/models/emotion_classification/classifier.py
+++ face_service/models/emotion_classification/classifier.py
@@ -1,22 +1,8 @@
 from PIL import Image
-from torchvision.transforms import Normalize
-from transformers import ( 
-    ViTImageProcessor,  
-    ViTForImageClassification,
-)
+from transformers import ViTForImageClassification
 from transformers import pipeline
 
 model_path = './model/'
-
-processor = ViTImageProcessor.from_pretrained(model_path)
-image_mean, image_std = processor.image_mean, processor.image_std
-normalize = Normalize(mean=image_mean, std=image_std)
-
-labels_list = ['sad', 'disgust', 'angry', 'neutral', 'fear', 'surprise', 'happy'] 
-
-model = ViTForImageClassification.from_pretrained(model_path, num_labels=len(labels_list))
-model.config.id2label = {ix: label for ix, label in enumerate(labels_list)}
-model.config.label2id = {label: ix for ix, label in enumerate(labels_list)}
 
 pipe = pipeline('image-classification', model=model_path, device=0)
 
--- face_service/models/emotion_classification/download_model.py
+++ face_service/models/emotion_classification/download_model.py
@@ -1,14 +1,9 @@
-from transformers import (
-    ViTImageProcessor,
-    ViTForImageClassification,
-)
+from transformers import ViTForImageClassification
 
 model_str = "dima806/facial_emotions_image_detection"
 
 labels_list = ['sad', 'disgust', 'angry', 'neutral', 'fear', 'surprise', 'happy']
 
-processor = ViTImageProcessor.from_pretrained(model_str)
 model = ViTForImageClassification.from_pretrained(model_str, num_labels=len(labels_list))
 
 model.save_pretrained('./model')
-processor.save_pretrained('./model')
